cef3782c242551906a7a36b5c90b99145bab772d
liamdevine <liamdevine@078c8882-b850-11de-b71e-ab0a53e69959>
2010-05-27 23:28:41 +0000

    Fixed vs2010 project generation adjusted tests to use generation .bat

--- build_scripts/vs2010x86_tests.bat
+++ build_scripts/vs2010x86_tests.bat
@@ -1,41 +1,8 @@
 CALL cleanVS10.bat
-
+CALL premake_vs2010.bat
 cd ..
 
-:: build the solution
-::premake4.exe clean 
-
-::if exist oolua.vcxproj.filters del oolua.vcxproj.filters
-::if exist oolua.vcxproj del oolua.vcxproj
-::if exist oolua.vcxproj del oolua.vcxproj.user
-
-::cd unit_tests 
-::if exist test.unit.vcxproj.filters del test.unit.vcxproj.filters
-::if exist test.unit.vcxproj del test.unit.vcxproj
-::if exist test.unit.vcxproj.user del test.unit.vcxproj.user
-
-::cd tests_may_fail
-::if exist tests_may_fail.vcxproj.filters del tests_may_fail.vcxproj.filters
-::if exist tests_may_fail.vcxproj del tests_may_fail.vcxproj
-::if exist tests_may_fail.vcxproj.user del tests_may_fail.vcxproj.user
-
-::cd ..\..
-
-
-premake4.exe vs2008 windows
-
 if not exist .\build_logs  md .\build_logs
-
-
-
-CALL "C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools\vcupgrade.exe" oolua.vcproj
-CALL "C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools\vcupgrade.exe" .\\unit_tests\\test.unit.vcproj
-CALL "C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools\vcupgrade.exe" .\\unit_tests\\test.unit.using_exceptions.vcproj
-CALL "C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools\vcupgrade.exe" .\\unit_tests\\tests_may_fail\\tests_may_fail.vcproj
-CALL "C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools\vcupgrade.exe" .\\unit_tests\\string_is_integral\\string_is_integral.vcproj
-CALL "C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools\vcupgrade.exe" .\\file_generator\\file_generator.vcproj
-CALL "C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools\vcupgrade.exe" .\\profile\\profile.vcproj
-
 
 :: start devenv
 CALL "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
