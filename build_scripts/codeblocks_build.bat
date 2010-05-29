d6032643e9b00e788819eeca0c48de3481fa7564
liamdevine <liamdevine@078c8882-b850-11de-b71e-ab0a53e69959>
2010-05-29 23:38:46 +0000

    Changed codeblocks unit test bat files to compile all tests. Exception unit test using debug config is broken see http://code.google.com/p/oolua/issues/detail?id=16

--- build_scripts/codeblocks_build.bat
+++ build_scripts/codeblocks_build.bat
@@ -1,3 +1,5 @@
+
+cd ..
 premake4.exe clean
 premake4.exe codeblocks windows
 
@@ -13,8 +15,10 @@ md local_install\oolua
 CALL "C:\Program Files\CodeBlocks\codeblocks.exe" --build oolua.cbp --target="Debug" > .\build_logs\oolua_codeblocks_debug.log
 CALL "C:\Program Files\CodeBlocks\codeblocks.exe" --build oolua.cbp --target="Release" > .\build_logs\oolua_codeblocks_release.log
 
-copy %~dp0include\*.h %~dp0local_install\oolua\*.h 
+copy .\include\*.h .\local_install\oolua\*.h  
 copy .\bin\Debug\*.a .\local_install\
 copy .\bin\Release\*.a .\local_install\
 
 premake4.exe clean
+
+cd build_scripts
