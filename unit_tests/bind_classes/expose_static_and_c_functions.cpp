393c1dc2b56fc3d90ec22b1b654ea237698f5ffc
liamdevine <liamdevine@078c8882-b850-11de-b71e-ab0a53e69959>
2010-05-31 20:37:22 +0000

    support for c and static functions with no parameters on mscv using OOLUA_C_FUNCTION

--- unit_tests/bind_classes/expose_static_and_c_functions.cpp
+++ unit_tests/bind_classes/expose_static_and_c_functions.cpp
@@ -5,7 +5,9 @@ EXPORT_OOLUA_NO_FUNCTIONS(ClassHasStaticFunction)
 
 int oolua_ClassHasStaticFunction_static_function(lua_State* l)
 {
-	OOLUA_C_FUNCTION_0(void,ClassHasStaticFunction::static_function)
+	//OOLUA_C_FUNCTION_0(void,ClassHasStaticFunction::static_function)
+	OOLUA_C_FUNCTION(void,ClassHasStaticFunction::static_function)
+	
 }
 
 int oolua_ClassHasStaticFunction_static_function_int(lua_State* l)
