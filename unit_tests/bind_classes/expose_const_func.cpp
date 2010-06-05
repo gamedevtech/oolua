fb229f0a05827699a46a4f6ae2c6f7b992b94a02
liamdevine <liamdevine@078c8882-b850-11de-b71e-ab0a53e69959>
2010-06-05 17:56:00 +0000

    removed dead code from refactored unit tests

--- unit_tests/bind_classes/expose_const_func.cpp
+++ unit_tests/bind_classes/expose_const_func.cpp
@@ -1,8 +1,5 @@
 #include "expose_const_func.h"
-/*
-EXPORT_OOLUA_FUNCTIONS_2_NON_CONST(C_simple,func,set_int)
-EXPORT_OOLUA_FUNCTIONS_3_CONST(C_simple,bar,get_int,bar1)
-*/
+
 
 EXPORT_OOLUA_FUNCTIONS_1_NON_CONST(Constant,cpp_func)
 EXPORT_OOLUA_FUNCTIONS_1_CONST(Constant,cpp_func_const)
--- unit_tests/bind_classes/expose_const_func.h
+++ unit_tests/bind_classes/expose_const_func.h
@@ -4,18 +4,6 @@
 #	include "cpp_const_funcs.h"
 #	include "oolua.h"
 
-/*
-OOLUA_CLASS_NO_BASES(C_simple)
-OOLUA_NO_TYPEDEFS
-OOLUA_MEM_FUNC_1(void,set_int,int const & )
-OOLUA_MEM_FUNC_0_CONST(int,get_int)
-OOLUA_MEM_FUNC_0(void,func)
-OOLUA_MEM_FUNC_0_CONST(C_simple const,bar)
-OOLUA_MEM_FUNC_0_CONST(C_simple const* ,bar1)
-	OOLUA_ONLY_DEFAULT_CONSTRUCTOR
-OOLUA_CLASS_END
-*/
-
 OOLUA_CLASS_NO_BASES(Constant)
 	OOLUA_NO_TYPEDEFS
 	OOLUA_ONLY_DEFAULT_CONSTRUCTOR
