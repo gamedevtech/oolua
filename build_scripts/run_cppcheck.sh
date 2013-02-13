cc0dcaeeceeade89a4efc3eac03589d4dc35dcca
liam.list@gmail.com <liam.list@gmail.com@078c8882-b850-11de-b71e-ab0a53e69959>
2013-02-13 01:03:43 +0000

    update cppcheck script updating flags which are no longer valid

--- build_scripts/run_cppcheck.sh
+++ build_scripts/run_cppcheck.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
 
-cppcheck -v -a -s -I ../include/ ../src/  2> ../build_logs/oolua_cppcheck.txt
-#following tests everything including unit test code
-#./cppcheck -v -a -s -I ./include/ ./src/ ./unit_tests/  2> oolua_cppcheck.txt
+
+if [ ! -d ../build_logs ]; then
+	mkdir ../build_logs
+fi
+
+cppcheck --verbose --enable=all -I ../include/ ../src/  2> ../build_logs/oolua_cppcheck.txt
+#following tests everything including unit test code. Note this will generate false errors and false style errors
+#cppcheck --verbose --enable=all -I ../include/ ../src/ ../unit_tests/  2> ../build_logs/oolua_cppcheck.txt
