62828c19a922b96d898ba409c72daeb4ea35943a
liam.list@googlemail.com <liam.list@googlemail.com@078c8882-b850-11de-b71e-ab0a53e69959>
2013-09-09 20:41:29 +0000

    Add new format tests for false integral function returns

---
+++ unit_tests/bind_classes/expose_false_integral_function_returns.cpp
@@ -0,0 +1,20 @@
+
+#include "expose_false_integral_function_returns.h"
+
+OOLUA_EXPORT_FUNCTIONS(FunctionRefReturn
+					   ,value
+					   ,ref
+					   ,refConst)
+OOLUA_EXPORT_FUNCTIONS_CONST(FunctionRefReturn)
+
+OOLUA_EXPORT_FUNCTIONS(TableRefReturn
+					   ,value
+					   ,ref
+					   ,refConst)
+OOLUA_EXPORT_FUNCTIONS_CONST(TableRefReturn)
+
+OOLUA_EXPORT_FUNCTIONS(TableObjectReturn
+					   ,value
+					   ,ref
+					   ,refConst)
+OOLUA_EXPORT_FUNCTIONS_CONST(TableObjectReturn)
