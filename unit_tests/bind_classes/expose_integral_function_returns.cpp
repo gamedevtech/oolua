efa46635537c3e4a3cb301bebbf2af6fca55cff5
liam.list@gmail.com <liam.list@gmail.com@078c8882-b850-11de-b71e-ab0a53e69959>
2013-09-17 22:46:04 +0000

    Adds missing tests for functions returning C strings

---
+++ unit_tests/bind_classes/expose_integral_function_returns.cpp
@@ -0,0 +1,11 @@
+
+#include "expose_integral_function_returns.h"
+
+
+OOLUA_EXPORT_FUNCTIONS(CStringFunctionReturn
+					   ,ptr
+					   ,ptrConst
+					   )
+
+OOLUA_EXPORT_FUNCTIONS_CONST(CStringFunctionReturn)
+
