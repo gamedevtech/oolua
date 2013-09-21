1f34a61028435da10061a074cf01adf4981be9a6
liam.list@googlemail.com <liam.list@googlemail.com@078c8882-b850-11de-b71e-ab0a53e69959>
2013-09-21 15:08:39 +0000

    Make Lua version tests work with xcode and gnu. Added extra command line parameter to cppcheck to enable finding Lua headers.

--- dev/lua_versions_test.sh
+++ dev/lua_versions_test.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+if [ "$1" != 'gnu' -a  "$1" != 'xcode' ]; then
+	echo "Unknown build requested \""$1"\". Only gnu and xcode supported"
+	exit 2
+fi
+test_script=$1
+
+
 if [ $EUID != 0 ]; then
-    echo  "WARNING:
-  	This file is not for general purpose usage and is required to have elevated privileges.
-	It will create symlinks for header directories and library names so that it can run
-	tests for multiple versions of Lua without modification to the generation files, tests
-	and the OOLua library. If you do run this file with the privileges it requires it may 
-	well remove the Lua library and headers from your system."
+	echo  "WARNING:
+	This file is not for general purpose usage and is required to have
+	elevated privileges. It will create symlinks for header directories
+	and library names so that it can run tests for multiple versions of
+	Lua without modification to the generation files, tests and the
+	OOLua library.
+	If you do run this file with the privileges it requires it may well
+	remove the Lua library and headers from your system."
 	exit 2
 fi
 
@@ -20,27 +29,30 @@ function setup_headers_and_library()
 }
 
 time_start=$(date +%s)
+cd ../build_scripts
 
 setup_headers_and_library lua51 liblua-5.1.5.a
 echo "Running Lua 5.1.5 tests"
-./xcode_tests.sh
+./${test_script}_tests.sh
 
 setup_headers_and_library lua52 liblua-5.2.2.a
 echo "Running Lua 5.2.2 tests"
-./xcode_tests.sh
+./${test_script}_tests.sh
 
-setup_headers_and_library luajit-2.0 libluajit-5.1.2.0.0.a
+#setup_headers_and_library luajit-2.0 libluajit-5.1.2.0.0.a
+setup_headers_and_library luajit-2.0 libluajit-5.1.2.0.1.a
 #OSX x86_64 executables which use LuaJIT require rebasing so that the JIT allocator can use the lowest 2GB
 export LUAJIT_REBASE=1
 echo "Running LuaJIT 2.0.0 tests"
-./xcode_tests.sh
+./${test_script}_tests.sh
 unset LUAJIT_REBASE
+
 
 <<ThisRequiresCppUnitAndGoogleMockToBeBuiltForx86
 setup_headers_and_library lua51 libluajit-5.1.1.1.8.a
 echo "Running LuaJIT 1.1.8 tests"
 export LUAJIT_32BIT=1
-./xcode_tests.sh
+./${test_script}_tests.sh
 unset LUAJIT_32BIT
 ThisRequiresCppUnitAndGoogleMockToBeBuiltForx86
 
@@ -55,4 +67,6 @@ setup_headers_and_library lua52 liblua-5.2.2.a
 
 #remove all traces whilst we are still sudo
 sudo chmod -R 775 ../build_logs
+
+cd ../dev
 
